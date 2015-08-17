9af8530eeba752e6a36da05b650479a3b5e6ef40
Guillaume Horel <guillaume.horel@serenitascapital.com>
2015-08-17 17:58:29 -0400

    Fix bug in DTLS client Hello use_srtp extension parsing

--- library/ssl_srv.c
+++ library/ssl_srv.c
@@ -578,7 +578,7 @@ static int ssl_parse_use_srtp_ext( ssl_context *ssl,
 		/* parse the extension list values are defined in http://www.iana.org/assignments/srtp-protection/srtp-protection.xhtml */
         for( j=0; j<profile_length; j+=2 )
         {
-            uint16_t protection_profile_value = buf[j]<<8 | buf[j+1];
+            uint16_t protection_profile_value = buf[j+2]<<8 | buf[j+3]; /* +2 to skip the length field */
 
             switch ( protection_profile_value )
             {
